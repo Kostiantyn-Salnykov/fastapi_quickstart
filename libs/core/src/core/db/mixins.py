9264abc999817b0ae0a80d5144034d483244211d
Kostiantyn Salnykov <kostiantyn.salnykov@gmail.com>
2025-01-26 01:08:53 +0200

    Intermediate commit

---
+++ libs/core/src/core/db/mixins.py
@@ -0,0 +1,106 @@
+__all__ = (
+    "BaseTableModelMixin",
+    "CreatedAtMixin",
+    "CreatedUpdatedMixin",
+    "UUIDMixin",
+    "UpdatedAtMixin",
+)
+import datetime
+import re
+import uuid
+
+import uuid_extensions
+from core.annotations import SchemaType
+from core.helpers import get_utc_timezone
+from sqlalchemy import TIMESTAMP
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import (
+    Mapped,
+    declarative_mixin,
+    declared_attr,
+    mapped_column,
+    validates,
+)
+from sqlalchemy.sql import func
+
+
+@declarative_mixin
+class BaseTableModelMixin:
+    """Mixin for rewrite table name magic method."""
+
+    pattern = re.compile("((?<=[a-z0-9])[A-Z]|(?!^)[A-Z](?=[a-z]))")
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        """Dynamic attribute to create name for table in PostgreSQL.
+
+        Examples:
+            class Users -> "users";
+            class WishList -> "wish_list";
+            class WishTag -> "wish_tag";
+        """
+        return cls.pattern.sub(r"_\1", cls.__name__).lower()
+
+    def __str__(self) -> str:
+        """Default Human representation for Model."""
+        return self.__repr__()
+
+    def to_schema(self, schema_class: SchemaType) -> SchemaType:
+        return SchemaType.model_validate(self, schema=schema_class, from_attributes=True)
+
+
+@declarative_mixin
+class UUIDMixin:
+    """Mixin for rewrite integer id field to uuid4 id field."""
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        default=uuid_extensions.uuid7,
+        primary_key=True,
+    )
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(id="{self.id}")'
+
+
+@declarative_mixin
+class CreatedAtMixin:
+    """Mixin for adding "created_at" field."""
+
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        server_default=func.current_timestamp(),
+        nullable=False,
+    )
+
+    @validates("created_at")
+    def validate_tz_info(self, _: str, value: datetime.datetime) -> datetime.datetime:
+        if value.tzinfo is None:
+            value = value.replace(tzinfo=get_utc_timezone())
+        return value
+
+
+@declarative_mixin
+class UpdatedAtMixin:
+    """Mixin for adding "updated_at" field."""
+
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        server_default=func.now(),
+        server_onupdate=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
+
+    @validates("updated_at")
+    def validate_tz_info(self, _: str, value: datetime.datetime) -> datetime.datetime:
+        if value.tzinfo is None:
+            value = value.replace(tzinfo=get_utc_timezone())
+        return value
+
+
+@declarative_mixin
+class CreatedUpdatedMixin(CreatedAtMixin, UpdatedAtMixin):
+    """Mixin for adding "updated_at" and "created_at" fields."""
+
+    ...
---
+++ libs/core/src/core/db/tables.py
@@ -0,0 +1,64 @@
+from core.db.bases import Base
+from core.db.mixins import CreatedUpdatedMixin, UUIDMixin
+from sqlalchemy import VARCHAR
+from sqlalchemy.dialects.postgresql import JSONB, TEXT
+from sqlalchemy.orm import Mapped, mapped_column
+from starlette.authentication import BaseUser
+
+from src.api.users.enums import UserStatuses
+
+
+class User(Base, UUIDMixin, CreatedUpdatedMixin, BaseUser):
+    """User class and `user` table declaration.
+
+    Keyword Args:
+        first_name (str): First name of user.
+        last_name (str): Last name of user.
+        email (str): User's email.
+        password_hash (str): Hashed value of password.
+        status (str): Current status of user.
+
+        groups (list[Group]): Groups that are assigned to user.
+        roles (list[Role]): Roles that assigned to user.
+        permissions (list[Role]): Permissions that assigned to user.
+    """
+
+    first_name: Mapped[str] = mapped_column(VARCHAR(length=128), nullable=False)
+    last_name: Mapped[str] = mapped_column(VARCHAR(length=128), nullable=False)
+    email: Mapped[str] = mapped_column(VARCHAR(length=255), nullable=False, index=True, unique=True)
+    password_hash: Mapped[str] = mapped_column(VARCHAR(length=1024), nullable=False)
+    status: Mapped[str] = mapped_column(VARCHAR(length=64), default=UserStatuses.UNCONFIRMED.value, nullable=False)
+    settings: Mapped[dict] = mapped_column(
+        JSONB(none_as_null=True, astext_type=TEXT()),
+        default=dict,
+        nullable=False,
+        doc="User's settings.",
+        comment="TEST COMMENT.",
+    )
+
+    def __repr__(self) -> str:
+        """Representation of User."""
+        return f'{self.__class__.__name__}(email="{self.email}", password_hash="...", status="{self.status}")'
+
+    @property
+    def is_authenticated(self) -> bool:
+        """User is authenticated automatically."""
+        return True
+
+    @property
+    def display_name(self) -> str:
+        """Concatenate full name of user.
+
+        Returns:
+            - (str): Full name of user.
+        """
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def identity(self) -> str:
+        """Get user UUID and convert it to string.
+
+        Returns:
+            - (str): User's id converted to string.
+        """
+        return str(self.id)
